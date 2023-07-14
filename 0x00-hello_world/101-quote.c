e967dda1727e46da678c5ab0f42c8a959d2a16b9
SomaaliDraphicdesiner <abdirshid32@gamil.com>
2023-07-14 13:44:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,22 @@
+100-intel
+
+#!/bin/bash
+gcc -S -masm=intel $CFILE
+
+101-quote.c
+
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Kor Par, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
